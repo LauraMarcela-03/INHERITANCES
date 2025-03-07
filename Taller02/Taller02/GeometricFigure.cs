24140034db3bd830ada9eb568f6bd3d954bad8b9
Laura Valle <lauramarcela.vu@gmail.com>
2025-03-07 18:27:27 -0500

    End

--- Taller02/Taller02/GeometricFigure.cs
+++ Taller02/Taller02/GeometricFigure.cs
@@ -15,7 +15,7 @@ public abstract class GeometricFigure
     //Methods
     public override string ToString()
     {
-        return $"{Name,-19} =>.....: {GetArea(),12:F5}    {GetPerimeter(),12:F5}." ;
+        return $"{Name,-19} =>  Area.....: {GetArea(),12:F5}    Perimeter: {GetPerimeter(),12:F5}." ;
     }
 
     public abstract double GetArea();
